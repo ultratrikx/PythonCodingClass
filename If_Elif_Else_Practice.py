391bbb7814b2dfd8bb3a6040abd514ff51782204
ultratrikx <64563315+ultratrikx@users.noreply.github.com>
2021-07-18 19:46:25 -0400

    If/Elif/Else Practice

---
+++ If_Elif_Else_Practice.py
@@ -0,0 +1,10 @@
+colour = 'white'
+
+if colour == 'yellow':
+    print('yay!')
+elif colour == 'white':
+    print('ooh plain white')
+elif colour == 'black':
+    print('dark black, very nice')
+else:
+    print('i do not like this colour')
