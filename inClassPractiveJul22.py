1285fd108d8c0b1ce4a7952f8caffe78eeff9324
ultratrikx <64563315+ultratrikx@users.noreply.github.com>
2021-07-22 11:09:45 -0400

    Jul 22 In Class Follow Along

---
+++ inClassPractiveJul22.py
@@ -0,0 +1,64 @@
+# a = 1
+a = 1 + 2j
+b = 2
+c = (a+b)
+print(type(c))
+print(type(b))
+print(type(a))
+
+d = True+True
+print(d)
+
+e = a.imag
+f = a.real
+print(e)
+print(type(e))
+print(f)
+print(type(f))
+
+g = a.conjugate()
+print(g)
+print(type(g))
+
+"H"+"e"+"l"+"l"+"o"
+
+50 - 5 * 6
+
+5 ** 3
+
+17 % 2
+5*5*5*5*5 == 5**5
+
+A, B, C = 10, 20.5, 30
+print(A+B)
+print((A+B)*C)
+print(C*C == (A+B)*C)
+
+
+x = 1.0 - 2.0j
+
+print("hello, \"hello\"")
+print("c:\\name\py.3.9")
+print(r"c:\name\py.3.9")
+print("""this is 
+a sample
+multiline 
+string or 
+remarks""")
+
+s = "Medhansh, Varshitha, Dharika, Rish, Raghav, Thanvika, Rohanth, and Krishna"
+print(len(s))
+
+print('###   Medhansh, Varshitha, Dharika, Rish, Raghav, Thanvika, Rohanth, and Krishna   ###')
+
+j = '###'
+n = '   '
+l = j+n + s + n + j
+print(l)
+
+
+s1 = "Sample String"
+print(s1 + ' #01')
+'Sample String #01'
+
+print(s1 + str(' #0') + str(1))
