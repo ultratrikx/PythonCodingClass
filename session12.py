09b5f2a33d17f8b38f07cc87b6445f031cdd26c7
ultratrikx <64563315+ultratrikx@users.noreply.github.com>
2021-07-27 11:02:57 -0400

    Memory Management and Slicing

---
+++ session12.py
@@ -0,0 +1,17 @@
+name1 = 'Rohanth Marem'
+print(id(name1[-1]))
+print(id(name1[2]))
+print(name1[1] + name1[3:7])
+
+s = 'Welcome'
+
+print(s[:7:7+1])
+print(s[:7:])
+print(s[::2])
+print(s[::3])
+print(s[::-1])
+
+print(name1[::-1])
+print(s[:4])
+print(s[::100] + s[3:7])
+print(s[6:2:1])
