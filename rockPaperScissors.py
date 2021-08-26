8e022333c3fa10727198f187c9be2e05acd7cebb
Rohanth Marem <64563315+ultratrikx@users.noreply.github.com>
2021-08-26 08:13:19 -0700

    final session

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,34 +1,34 @@
-import random
+from random import choice
 
 choices = ("rock", "paper", "scissors")
 print("Rock crushes scissors. Scissors cut paper. Paper covers rock")
 
-
-player = input('Enter your choice, Rock, Paper or Scissors. Type quit to leave the game.')
-
+player = input(
+    'Enter your choice, Rock, Paper or Scissors. Type quit to leave the game.')
+player = player.lower()
 while player != "quit":
-    player = player.lower()
-    computer = random.choice(choices)
-    print("You chose " +player+ ", and the computer chose " +computer+".")
+    computer = choice(choices)
+    print("You chose " + player + ", and the computer chose " + computer+".")
     if player == computer:
         print("its a tie")
     elif player == "rock":
-        if computer == "paper":
-            print('computer wins')
+        if computer == "scissors":
+            print('you win')
         else:
-            print("you won")
+            print("computer won")
     elif player == "paper":
         if computer == 'rock':
             print('you won!!')
         else:
             print('computer wins')
     elif player == "scissors":
-        if computer == 'rock':
-            print("computer won")
+        if computer == 'paper':
+            print("you won")
         else:
-            print('you won')
+            print('computer won')
     else:
         print('error occured')
-print()
-player = input('Enter your choice, Rock, Paper or Scissors. Type quit to leave the game.')
 
+    print()
+    player = input(
+        'Enter your choice, Rock, Paper or Scissors. Type quit to leave the game.')
